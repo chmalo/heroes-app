a3517364d5fb266ded022130abc951f9acc749a6
Chmalo <chmalo.f@gmail.com>
2022-05-27 08:39:32 -0400

    feat: login and logout

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,8 +1,12 @@
-import { Link, NavLink } from "react-router-dom"
+import { Link, NavLink, useNavigate } from "react-router-dom"
 
 export const Navbar = () => {
+  const navigate = useNavigate()
+
   const handleLogout = () => {
-    console.log("logout")
+    navigate("/login", {
+      replace: true,
+    })
   }
 
   return (
